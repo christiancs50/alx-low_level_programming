0aff3f27690183b3f82e6d981b1f144f8cb11e9f
christian <christian.gaoso@gmail.com>
2023-06-16 04:38:33 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print - String
+ *
+ * Return (0);
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+	

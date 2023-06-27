f4587f888364f3195a8aa2a1d3482fc86e4623ec
christian <christian.gaoso@gmail.com>
2023-06-26 22:34:35 -0700

    I can only go one way.I have not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <sdtio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: character
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int number = 0;
+
+	while (*s)
+	{
+		s++;
+		number++;
+	}
+	while (number)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+	_putchar('\n'
+}

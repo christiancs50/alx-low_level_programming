7563f8780defae483df2f1a70750281dcf8d7e9a
christian <christian.gaoso@gmail.com>
2023-07-08 15:46:28 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,17 +17,19 @@ void times_table(void)
 			c = a * b;
 
 			if (b != 0)
-				_putchar (' ');
-
+			_putchar(' ');
 			if (c <= 9)
+			{
+				_putchar(' ');
 				_putchar(c + '0');
+			}
 			else
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 			}
 			if (b < 9)
-				_putchar(' ');
+				_putchar(',');
 			else
 				_putchar('\n');
 		}

336b7b354de6bf277e01d9f6dd1a10ffa092f4ef
christian <christian.gaoso@gmail.com>
2023-08-28 05:19:57 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,16 @@
 
 /**
  *add - entry point
- *Desription: adds two integers
+ *Description: adds two integers
+ *@num1: first number
+ *@num2: second number
  *Return: sum
  */
 
 int add(int num1, int num2)
 {
 	int sum;
-	sum = num1 + num2;
+
+	sum = (num1 + num2);
 	return (sum);
 }

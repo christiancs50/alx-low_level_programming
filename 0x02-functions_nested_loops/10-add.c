f693f0bcd0d3ba4043604b70b3c9fc5a3d8fdc1b
christian <christian.gaoso@gmail.com>
2023-08-28 04:58:00 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - entry point
+ *Desription: adds two integers
+ *Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+	sum = num1 + num2;
+	return (sum);
+}

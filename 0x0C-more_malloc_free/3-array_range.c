0f635b8bc65cb08f218c278338f6728ca7587a91
christiancs50 <christian.gaoso@gmail.com>
2023-09-08 06:52:16 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,15 +15,12 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-
 	size = max - min + 1;
 	ptr = malloc(sizeof(int) * size);
 
 	if (ptr == NULL)
 		return (NULL);
-
 	for (i = 0; i < size; i++)
 		ptr[i] = min++;
-
 	return (ptr);
 }

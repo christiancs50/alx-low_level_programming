3d60c414e6963b15f514c564310637e5636f9305
“christiancs50” <christian.gaoso@gmail.com>
2023-09-26 07:36:09 -0700

    headerfiles

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - a structure containing dog info
  * @name: name of dog
@@ -15,5 +19,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */

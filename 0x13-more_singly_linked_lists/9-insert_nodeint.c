e7b9768ff46b9da5374dfd9b577c24e107474c1f
“christiancs50” <christian.gaoso@gmail.com>
2023-09-26 19:47:59 -0700

    9. Insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given postn in a linked list
+ * @head: Double pointer to the head of the list
+ * @idx: Index where the new node should be inserted
+ * @n: Value for the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+}
